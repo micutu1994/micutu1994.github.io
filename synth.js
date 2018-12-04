174ecc3f7831607a681190d97554fc76dc30b006
micutu1994 <44406437+micutu1994@users.noreply.github.com>
2018-12-04 17:49:37 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,9 +1,9 @@
 
 document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.1";
-
+var synth = window.speechSynthesis;
 function afiseaza_voci()
 {
-	var synth = window.speechSynthesis;
+	
 	var lista_voci = synth.getVoices();
 	
 	for (var i = 0; i < lista_voci.length; i++)
