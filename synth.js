6fc3afc93a519d9cdf2ee053b3500a65fb3b1ec3
micutu1994 <44406437+micutu1994@users.noreply.github.com>
2018-12-04 17:46:41 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,15 +1,12 @@
-document.getElementById("id_logic_version").innerHTML="logic version:"
+
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.1";
 
 function afiseaza_voci()
-
 {
+	var synth = window.speechSynthesis;
+	var lista_voci = synth.getVoices();
 	
-	var synth = window.speachSyntesis;
-	var lista_voci = synt.getVoices();
-	for (var i=0;i<lista_voci.length;i++)
-		document.getElementById("id_voci ").innerHTML += lista_voci[i].lang + " " + lista_voci[i].name +"<br>";
-	
-	
-	
-	
+	for (var i = 0; i < lista_voci.length; i++)
+		document.getElementById("id_voci").innerHTML +=
+				lista_voci[i].lang + " " + lista_voci[i].name + "<br>";
 }
