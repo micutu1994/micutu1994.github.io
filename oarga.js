8b436843160eb53311e5e073311cad90d230a5f8
micutu1994 <44406437+micutu1994@users.noreply.github.com>
2019-01-20 21:15:38 +0200

    Update oarga.js

--- oarga.js
+++ oarga.js
@@ -1,5 +1,5 @@
 document.getElementById("id_business_version").innerHTML = "Business version =2018.01.20";
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.13.0";
+
 window.addEventListener("deviceorientation",on_device_orientation);
 /*window.addEventListener("devicemotion",on_device_motion);*/
 function on_device_orientation(e)
@@ -9,38 +9,3 @@ function on_device_orientation(e)
 	document.getElementById("id_x").innerHTML ="x " + Math.round(e.beta * 100)/100;
 }
 
-function on_device_motion(e)
-{
-	document.getElementById("id_acc_x").innerHTML="acc x =" + Math.round(e.acceleration.x * 100)/100;
-	document.getElementById("id_acc_y").innerHTML="acc y =" + Math.round(e.acceleration.y * 100)/100;
-	document.getElementById("id_acc_z").innerHTML="acc z =" + Math.round(e.acceleration.z * 100)/100;
-	
-	document.getElementById("id_acc_g_x").innerHTML="acc g x =" + Math.round(e.accelerationIncludingGravity.x * 100)/100;
-	document.getElementById("id_acc_g_y").innerHTML="acc g y =" + Math.round(e.accelerationIncludingGravity.y * 100)/100;
-	document.getElementById("id_acc_g_z").innerHTML="acc g z =" + Math.round(e.accelerationIncludingGravity.z * 100)/100;
-
-	var beta = Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
-	var gamma = - Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
-
-	document.getElementById("id_rot_x").innerHTML="beta=" + Math.round(beta * 100)/100;
-	document.getElementById("id_rot_y").innerHTML="gamma=" + Math.round(gamma * 100)/100;
-	
-}
-
-//------------------------
-function on_ok(stream)
-{
-	document.getElementById("id_video").srcObject = stream;
-}
-//------------------------
-function on_error(e)
-{
-	//console.log("eroare camera");
-	alert("eroare camera");
-}
-//------------------------
-function start()
-{
-	var p = {audio: true, video: true};
-	navigator.mediaDevices.getUserMedia(p).then(on_ok).catch(on_error);
-}
