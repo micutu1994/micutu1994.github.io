98e52fedb1603775421a7c98a2dfea55d2115b24
micutu1994 <44406437+micutu1994@users.noreply.github.com>
2019-01-20 20:56:21 +0200

    Update oarga.js

--- oarga.js
+++ oarga.js
@@ -8,22 +8,3 @@ function on_device_orientation(e)
 	document.getElementById("id_x").innerHTML ="x " + Math.round(e.beta * 100)/100;
 }
 
-/* function on_device_motion(e)
-{
-	document.getElementById("id_acc_x").innerHTML="acc x =" + Math.round(e.acceleration.x * 100)/100;
-	document.getElementById("id_acc_y").innerHTML="acc y =" + Math.round(e.acceleration.y * 100)/100;
-	document.getElementById("id_acc_z").innerHTML="acc z =" + Math.round(e.acceleration.z * 100)/100;
-	
-	document.getElementById("id_acc_g_x").innerHTML="acc g x =" + Math.round(e.accelerationIncludingGravity.x * 100)/100;
-	document.getElementById("id_acc_g_y").innerHTML="acc g y =" + Math.round(e.accelerationIncludingGravity.y * 100)/100;
-	document.getElementById("id_acc_g_z").innerHTML="acc g z =" + Math.round(e.accelerationIncludingGravity.z * 100)/100;
-
-	var beta = Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
-	var gamma = - Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
-
-	document.getElementById("id_rot_x").innerHTML="beta=" + Math.round(beta * 100)/100;
-	document.getElementById("id_rot_y").innerHTML="gamma=" + Math.round(gamma * 100)/100;
-	
-	
-	
-}   */
